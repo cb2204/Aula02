c7819708aed3b7fe04737f203b547cb27b5d3973
Caio Barros <caio.gsbarros@gmail.com>
2025-09-25 11:07:07 -0300

    criei o arquivo main para fazer teste e aprender a usar o if e else

---
+++ main.py
@@ -0,0 +1,19 @@
+# type error 
+
+
+#%%
+
+
+
+num = int(input('digite um número'))
+
+if isinstance(num, bool):
+    print('vc digitou o inteiro ', num)
+else: 
+    print('vc n digitou um inteiro')
+
+#
+#
+#
+
+# %%
